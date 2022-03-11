d79c6214a9f5e2b6314a67db5520d54e2a9d3174
nzwithee@gmail.com <Cartoon#01>
2022-03-10 20:21:42 -0600

    record save functionaity working as expected

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,50 +5,61 @@ var setDate = function (){
     
     //push date to page
     $("#currentDay").append(date);
-
 }
 
 //Set color of each timeblock
 var colorSet = function (){
     //Capture current hour of the day
     var currentHour = moment().hour();
-    console.log(currentHour)
 
     //Logic to loop through all blocks and set color
     $(".time-block").each(function (){
         //pull in the time for the block currently being assessed
         var time = $(this).attr("id");
-        console.log(time);
 
         //loop through to determine whether the block is past, present, or future
-        if (time === currentHour){
+        if (time == currentHour){
+            $(this).removeClass("past");
+            $(this).removeClass("future");
             $(this).addClass("present");
         }
         else if (time < currentHour){
+            $(this).removeClass("present");
+            $(this).removeClass("future");
             $(this).addClass("past");
         }
         else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
             $(this).addClass("future");
         }
 
     })
-
+    
 }
 
+//Save values from timeblock on save button click
 var saveEvent = function (){
     $(".saveBtn").on("click", function() {
-        var event = $(".description").val();
-        var time = $.parent().attr("id");
+        //get value of content from sibling div
+        var message = $(this).siblings(".description").val();
+        //get value of time from parent div
+        var time = $(this).parent().attr("id");
 
-        console.log(event);
+        console.log(message);
         console.log(time);
 
+        localStorage.setItem(time, message);
     })
 
+    //loop to display stored values
+    for (var i = 8; i <18; i++ ) {
+        var number = i;
+        $("#" + number + " .description").val(localStorage.getItem(number));
+    }
+
+    //$("#8 .description").val(localStorage.getItem("8"));
 }
-
-
-
 
 colorSet();
 setDate();
